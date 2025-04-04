d2ec16a429266d5542996e16925396dfc0f6a5ea
spider-calvin <calvindas7@gmail.com>
2025-04-05 03:27:34 +0530

    initial commit

---
+++ src/Context/StoreContext.jsx
@@ -0,0 +1,85 @@
+import { createContext, useEffect, useState } from "react";
+export const StoreContext = createContext(null);
+import axios from "axios"
+import {food_list as savedFoodList} from "../assets/assets"
+const StoreContextProvider = (props) => {
+  const[cartItems,setCartItems]= useState({})
+  // const url="http://localhost:4000";
+  const url = "https://cake-backend-7kov.onrender.com"
+  const[token,setToken]=useState("")
+  const[food_list,Setfood_list]=useState([])
+  const addToCart= async(itemId)=>{
+    if(!cartItems[itemId]){
+      setCartItems((prev)=>({...prev,[itemId]:1}))
+    }
+    else{
+      setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+    }
+    if(token){
+      await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+    }
+
+  }
+
+  const removeFromCart = async(itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    if(token){
+      await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+    }
+  }
+
+  const getTotalAmount=()=>{
+    let totalAmount=0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
+const fetchFoodList=async()=>{
+  const response = await axios.get(url+"/api/food/list");
+  if(response.data.data?.length > 0 ){
+    Setfood_list(response.data.data)
+  }else{
+    Setfood_list(savedFoodList)
+  }
+ 
+}
+
+const loadCardData = async(token)=>{
+  const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
+  setCartItems(response.data.cartData);
+}
+
+  useEffect(()=>{
+    
+    async function loadData(){
+      await fetchFoodList()
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+        await loadCardData(localStorage.getItem('token'))
+      }
+    }
+    loadData();
+  },[])
+
+  const contextValue = {
+    food_list,
+    addToCart,
+    cartItems,
+    removeFromCart,
+    setCartItems,
+    getTotalAmount,
+    url,
+    setToken,
+    token
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+export default StoreContextProvider;
---
+++ src/assets/assets.js
@@ -0,0 +1,376 @@
+
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Signature",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Cupcakes",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Cheesecakes",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Birthday",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Desserts",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Layered",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Bestselling",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Pastries",
+        menu_image: menu_8
+    }
+]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Red Velvet Signature",
+        image: food_1,
+        price: 1200,
+        description: "Our premium red velvet cake with cream cheese frosting and elegant decoration",
+        category: "Signature"
+    },
+    {
+        _id: "2",
+        name: "Black Forest Signature",
+        image: food_2,
+        price: 1350,
+        description: "Classic black forest cake with layers of chocolate, cherries, and whipped cream",
+        category: "Signature"
+    }, 
+    {
+        _id: "3",
+        name: "Hazelnut Signature",
+        image: food_3,
+        price: 1400,
+        description: "Rich hazelnut cake with premium chocolate ganache and hazelnut crunch",
+        category: "Signature"
+    }, 
+    {
+        _id: "4",
+        name: "Tiramisu Signature",
+        image: food_4,
+        price: 1500,
+        description: "Italian-inspired signature tiramisu with coffee-soaked layers and mascarpone",
+        category: "Signature"
+    }, 
+    {
+        _id: "5",
+        name: "Chocolate Cupcakes",
+        image: food_5,
+        price: 120,
+        description: "Rich chocolate cupcakes topped with chocolate buttercream and sprinkles",
+        category: "Cupcakes"
+    }, 
+    {
+        _id: "6",
+        name: "Vanilla Cupcakes",
+        image: food_6,
+        price: 110,
+        description: "Light vanilla cupcakes with vanilla buttercream and rainbow sprinkles",
+        category: "Cupcakes"
+    }, 
+    {
+        _id: "7",
+        name: "Red Velvet Cupcakes",
+        image: food_7,
+        price: 130,
+        description: "Moist red velvet cupcakes topped with cream cheese frosting",
+        category: "Cupcakes"
+    }, 
+    {
+        _id: "8",
+        name: "Assorted Cupcakes",
+        image: food_8,
+        price: 450,
+        description: "Box of 4 assorted cupcakes with different flavors and toppings",
+        category: "Cupcakes"
+    }, 
+    {
+        _id: "9",
+        name: "Classic Cheesecake",
+        image: food_9,
+        price: 950,
+        description: "New York style baked cheesecake with graham cracker crust",
+        category: "Cheesecakes"
+    }, 
+    {
+        _id: "10",
+        name: "Blueberry Cheesecake",
+        image: food_10,
+        price: 1050,
+        description: "Creamy cheesecake topped with homemade blueberry compote",
+        category: "Cheesecakes"
+    }, 
+    {
+        _id: "11",
+        name: "Chocolate Cheesecake",
+        image: food_11,
+        price: 1000,
+        description: "Decadent chocolate cheesecake on chocolate cookie base",
+        category: "Cheesecakes"
+    }, 
+    {
+        _id: "12",
+        name: "Mango Cheesecake",
+        image: food_12,
+        price: 1100,
+        description: "Seasonal Indian alphonso mango cheesecake with mango topping",
+        category: "Cheesecakes"
+    },
+    {
+        _id: "13",
+        name: "Children's Birthday Cake",
+        image: food_13,
+        price: 1800,
+        description: "Fun cartoon-themed cake perfect for children's birthday parties",
+        category: "Birthday"
+    },
+    {
+        _id: "14",
+        name: "Adult Birthday Cake",
+        image: food_14,
+        price: 2000,
+        description: "Elegant birthday cake with personalized design and decorations",
+        category: "Birthday"
+    }, 
+    {
+        _id: "15",
+        name: "Number Cake",
+        image: food_15,
+        price: 1600,
+        description: "Customized number-shaped cake with fruit toppings and edible flowers",
+        category: "Birthday"
+    }, 
+    {
+        _id: "16",
+        name: "Photo Cake",
+        image: food_16,
+        price: 1900,
+        description: "Birthday cake with edible printed photo of your choice",
+        category: "Birthday"
+    }, 
+    {
+        _id: "17",
+        name: "Chocolate Truffle Dessert",
+        image: food_17,
+        price: 180,
+        description: "Individual chocolate truffle dessert with rich ganache center",
+        category: "Desserts"
+    }, 
+    {
+        _id: "18",
+        name: "Tiramisu Cup",
+        image: food_18,
+        price: 220,
+        description: "Individual tiramisu dessert cup with coffee-soaked sponge and mascarpone",
+        category: "Desserts"
+    }, 
+    {
+        _id: "19",
+        name: "Fruit Tart",
+        image: food_19,
+        price: 250,
+        description: "Fresh seasonal fruit tart with custard filling and sweet pastry shell",
+        category: "Desserts"
+    }, 
+    {
+        _id: "20",
+        name: "Chocolate Mousse",
+        image: food_20,
+        price: 200,
+        description: "Silky chocolate mousse topped with chocolate shavings and berries",
+        category: "Desserts"
+    }, 
+    {
+        _id: "21",
+        name: "Three Layer Chocolate",
+        image: food_21,
+        price: 1400,
+        description: "Triple layer chocolate cake with dark, milk, and white chocolate layers",
+        category: "Layered"
+    }, 
+    {
+        _id: "22",
+        name: "Rainbow Cake",
+        image: food_22,
+        price: 1600,
+        description: "Stunning six-layer rainbow cake with vanilla buttercream",
+        category: "Layered"
+    }, 
+    {
+        _id: "23",
+        name: "Vanilla Berry Cake",
+        image: food_23,
+        price: 1500,
+        description: "Four layers of vanilla sponge with fresh berry compote between layers",
+        category: "Layered"
+    }, 
+    {
+        _id: "24",
+        name: "Butterscotch Layer Cake",
+        image: food_24,
+        price: 1300,
+        description: "Four-layer butterscotch cake with caramel buttercream and praline",
+        category: "Layered"
+    },
+    {
+        _id: "25",
+        name: "Chocolate Truffle Cake",
+        image: food_25,
+        price: 900,
+        description: "Our bestselling chocolate truffle cake with smooth ganache finish",
+        category: "Bestselling"
+    },
+    {
+        _id: "26",
+        name: "Butterscotch Crunch",
+        image: food_26,
+        price: 850,
+        description: "Popular butterscotch cake with caramel crunch topping",
+        category: "Bestselling"
+    }, 
+    {
+        _id: "27",
+        name: "Pineapple Cake",
+        image: food_27,
+        price: 800,
+        description: "Traditional favorite pineapple cake with fresh pineapple pieces",
+        category: "Bestselling"
+    }, 
+    {
+        _id: "28",
+        name: "Dutch Chocolate Cake",
+        image: food_28,
+        price: 950,
+        description: "Rich dutch chocolate cake with chocolate chips and fudge frosting",
+        category: "Bestselling"
+    }, 
+    {
+        _id: "29",
+        name: "Chocolate Éclair",
+        image: food_29,
+        price: 140,
+        description: "Classic chocolate éclair filled with vanilla pastry cream",
+        category: "Pastries"
+    }, 
+    {
+        _id: "30",
+        name: "Fruit Danish",
+        image: food_30,
+        price: 120,
+        description: "Buttery danish pastry topped with seasonal fruits",
+        category: "Pastries"
+    }, 
+    {
+        _id: "31",
+        name: "Almond Croissant",
+        image: food_31,
+        price: 160,
+        description: "Flaky croissant filled with almond cream and topped with sliced almonds",
+        category: "Pastries"
+    }, 
+    {
+        _id: "32",
+        name: "Cream Puff",
+        image: food_32,
+        price: 130,
+        description: "Light choux pastry puff filled with vanilla cream",
+        category: "Pastries"
+    }
+]
